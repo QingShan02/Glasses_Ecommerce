aa7dfe2abd33e76bba2dc9880cb14dab9557f489
Khang Ho <hohoangkhang18@gmail.com>
2023-05-24 18:38:38 +0700

    duoc roi nha

--- src/main/java/com/fpoly/controller/MainController.java
+++ src/main/java/com/fpoly/controller/MainController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +15,12 @@ import com.fpoly.form.PersonForm;
 import com.fpoly.model.Person;
 
 @Controller
+@RequestMapping("/home")
 public class MainController {
+	@GetMapping("/index")
+	public String homePage() {
+		return "zayshop/index";
+	}
 //	private static List<Person> persons = new ArrayList<Person>();
 //
 //	static {
