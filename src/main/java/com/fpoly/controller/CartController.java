ac9119c981da4e55c08b7b76dcad8aaf9032049b
Khang Ho <hohoangkhang18@gmail.com>
2023-06-08 17:11:45 +0700

    Sửa xong form chi tiết sản phẩm

--- src/main/java/com/fpoly/controller/CartController.java
+++ src/main/java/com/fpoly/controller/CartController.java
@@ -1,12 +1,31 @@
 package com.fpoly.controller;
 
+import com.fpoly.entity.Cart;
+import com.fpoly.utility.CookieUtility;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequestMapping("/cart")
 public class CartController {
+	@Autowired
+	CookieUtility cookie;;
+
 	@GetMapping("/cart")
 	public String homePage() {
 		return "/cart";
+	}
+
+	@PostMapping("/save")
+	public String saveProduct(Model model, Cart cart) {
+		cart.setUserId(Integer.parseInt(cookie.getValue("userId")));
+		System.out.println(cookie.getValue("userId"));
+		model.addAttribute("cart", cart);
+		return "redirect:/home/index";
 	}
 }
