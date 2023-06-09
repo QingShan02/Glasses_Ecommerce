a0087cd49e9b6b7411372743670239cd4e137341
QingShan02 <thanhson9112@gmail.com>
2023-06-09 12:40:26 +0700

    Finish ResetPassword and Merger branch An

--- src/main/java/com/fpoly/service/serviceImlp/MaillerSender.java
+++ src/main/java/com/fpoly/service/serviceImlp/MaillerSender.java
@@ -9,19 +9,21 @@ import com.fpoly.service.MailService;
 
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+
 @Service
-public class MaillerSender implements MailService{
+public class MaillerSender implements MailService {
 	@Autowired
 	JavaMailSender sender;
-	
+
 	@Override
-	public void send( String to, String body) throws MessagingException {
+	public void send(String to, String body) throws MessagingException {
 		MimeMessage message = sender.createMimeMessage();
 		MimeMessageHelper helper = new MimeMessageHelper(message, true, "utf-8");
 		helper.setFrom("Glasses-Shop");
 		helper.setTo(to);
 		helper.setSubject("Glasses Shop - Cửa Hàng Mắt Kính Thời Trang");
-		helper.setText("Welcome To Glasses Shop <br> Đây là mã xác thực của bạn: "+"<b>" +body+"</b>", true);
+		helper.setText("Welcome To Glasses Shop <br> Đây là mã xác thực của bạn: " + "<b>" + body + "</b> <br>"
+				+ "<p style='color:red'>" + "Lưu ý mã xác thực chỉ thời hạn trong 2 phút !" + "</p>", true);
 		helper.setReplyTo("Glasses-Shop");
 		sender.send(message);
 	}
