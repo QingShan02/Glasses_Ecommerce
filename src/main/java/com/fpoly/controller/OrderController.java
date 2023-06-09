089ddeb228776fa5dda9190ebe730a85856c579b
Khang Ho <hohoangkhang18@gmail.com>
2023-06-09 23:20:33 +0700

    fix order

--- src/main/java/com/fpoly/controller/OrderController.java
+++ src/main/java/com/fpoly/controller/OrderController.java
@@ -1,6 +1,7 @@
 package com.fpoly.controller;
 
 import com.fpoly.utility.CookieUtility;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -18,8 +19,9 @@ public class OrderController {
 
 
     @PostMapping("/save")
-    public String ordering(Model model, Order order) {
+    public String ordering(Model model, Order order, HttpServletRequest req) {
         orderService.ordering(order);
+        req.setAttribute("orderId", order.getId());
         model.addAttribute("orderId", order.getId());
 
         return "forward:/orderDetail/save";
--- src/main/java/com/fpoly/controller/OrderDetailController.java
+++ src/main/java/com/fpoly/controller/OrderDetailController.java
@@ -5,12 +5,14 @@ import com.fpoly.entity.Order;
 import com.fpoly.repository.CartRepository;
 import com.fpoly.service.OrderDetailService;
 import com.fpoly.utility.CookieUtility;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -25,12 +27,12 @@ public class OrderDetailController {
     CookieUtility cookie;
 
     @PostMapping("/save")
-    public String save(Model model) {
+    public String save(Model model, HttpServletRequest req) {
         Integer userId = Integer.parseInt(cookie.getValue("userId"));
-        Integer orderId = (Integer) model.getAttribute("orderId");
-        System.out.println(orderId);
+        Integer orderId = (Integer) req.getAttribute("orderId");
+
         List<Cart> carts = cartRepository.findByUserId(userId);
-        orderDetailService.saveAll(9, carts);
+        orderDetailService.saveAll(orderId, carts);
 
         return "redirect:/home/index";
     }
